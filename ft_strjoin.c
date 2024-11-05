e683b6813e8470fea76af9538d2c566dfd799b61
LLePep <140174761+LLePep@users.noreply.github.com>
2024-11-05 12:41:47 +0100

    Add files via upload

---
+++ ft_strjoin.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int ft_strlen(const char *s)
+{
+    int cpt;
+
+    while (*s != '\0')
+    {
+        cpt++;
+        s++;
+    }
+    
+    return (cpt);
+}
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    char *dest;
+    int cpt = 0;
+
+    dest = malloc(sizeof(char) * (ft_strlen (s1) + ft_strlen (s2) + 1));
+    if (dest == NULL)
+        return NULL;
+    while (*s1 != '\0')
+    {
+        dest[cpt] = *s1;
+        cpt++;
+        s1++;
+    }
+    while (*s2 != '\0')
+    {
+        dest[cpt] = *s2;
+        cpt++;
+        s2++;
+    }
+    dest[cpt] = '\0';
+    printf("%s", dest); 
+    return (dest);
+}
+
+int main()
+{
+    char *s1 = "Coucou";
+    char *s2 = "ca va ?";
+    ft_strjoin(s1, s2);
+    return(0);
+}
