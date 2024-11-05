335d5fdf62c106629aa27c4b6ad922c92dbf0e34
LLePep <140174761+LLePep@users.noreply.github.com>
2024-11-05 13:21:24 +0100

    Add files via upload

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,48 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpalabos <lpalabos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 12:48:55 by lpalabos          #+#    #+#             */
+/*   Updated: 2024/11/05 13:19:14 by lpalabos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <stdio.h>
 
-int ft_strlen(const char *s)
+int	ft_strlen(const char *s)
 {
-    int cpt;
+	int	cpt;
 
-    while (*s != '\0')
-    {
-        cpt++;
-        s++;
-    }
-    
-    return (cpt);
+	while (*s != '\0')
+	{
+		cpt++;
+		s++;
+	}
+	return (cpt);
 }
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char *dest;
-    int cpt = 0;
+	char	*dest;
+	int		cpt;
 
-    dest = malloc(sizeof(char) * (ft_strlen (s1) + ft_strlen (s2) + 1));
-    if (dest == NULL)
-        return NULL;
-    while (*s1 != '\0')
-    {
-        dest[cpt] = *s1;
-        cpt++;
-        s1++;
-    }
-    while (*s2 != '\0')
-    {
-        dest[cpt] = *s2;
-        cpt++;
-        s2++;
-    }
-    dest[cpt] = '\0';
-    printf("%s", dest); 
-    return (dest);
+	cpt = 0;
+	dest = malloc(sizeof(char) * (ft_strlen (s1) + ft_strlen (s2) + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (*s1 != '\0')
+	{
+		dest[cpt] = *s1;
+		cpt++;
+		s1++;
+	}
+	while (*s2 != '\0')
+	{
+		dest[cpt] = *s2;
+		cpt++;
+		s2++;
+	}
+	dest[cpt] = '\0';
+	return (dest);
 }
-
-int main()
+/*
+int     main()
 {
-    char *s1 = "Coucou";
-    char *s2 = "ca va ?";
-    ft_strjoin(s1, s2);
-    return(0);
-}
+	char *s1 = "Coucou";
+	char *s2 = "ca va ?";
+	ft_strjoin(s1, s2);
+	return(0);
+}*/
