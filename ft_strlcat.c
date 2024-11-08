8fbeac88022375a8d61fd7edf51f3cc97cba44c0
LLePep <140174761+LLePep@users.noreply.github.com>
2024-11-08 05:19:14 +0100

    Add files via upload

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -11,7 +11,9 @@
 /* ************************************************************************** */
 
 #include <string.h>
-
+#include <stdio.h>
+//la fonction verifie la longueuer de dest par rapport a src
+//Penser a size - 1 pour placer le /0
 static size_t	ft_strlen(const char *s)
 {
 	int	cpt;
@@ -22,17 +24,30 @@ static size_t	ft_strlen(const char *s)
 	return (cpt);
 }
 
-size_t	strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t lensrc;
-	
-	lensrc = strlen(src);
-	
-	return (lensrc);
+	size_t cpt;
+	size_t lendest;
+
+	lendest = ft_strlen(dest);
+	cpt = 0;
+	if (size <= lendest)
+        return size + ft_strlen(src);
+	while( ( lendest + cpt + 1 ) < size && src[cpt] != '\0')
+	{
+		dest[cpt + lendest] = src[cpt];
+		cpt++;
+	}
+	dest[cpt + lendest] = '\0';
+	return (ft_strlen(src) + lendest);
 }
 
-int	main()
-{
-	
-	return (0);
+int main() {
+    char dst[24] = "Bonjour, ";
+    char src[] = "tout le monde!";
+    
+    size_t n = ft_strlcat(dst, src, sizeof(dst));
+    printf("Chaine concatenee: %s\n", dst);
+    printf("Longueur de la chaine totale si assez d'espace: %zu\n", n);
+    return 0;
 }
